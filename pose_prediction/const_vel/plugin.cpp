d2b5615ef3d8c0646acde9a5f7138a4f3965a25d
Muhammad Huzaifa <huzaifa2@illinois.edu>
2022-05-09 01:13:27 -0500

    Add constant velocity pose predictor

--- pose_prediction/const_vel/plugin.cpp
+++ pose_prediction/const_vel/plugin.cpp
@@ -7,16 +7,16 @@
 
 using namespace ILLIXR;
 
-class const_accel_predictor_impl : public pose_prediction {
+class const_vel_predictor_impl : public pose_prediction {
 public:
-    const_accel_predictor_impl(const phonebook* const pb)
+    const_vel_predictor_impl(const phonebook* const pb)
         : sb{pb->lookup_impl<switchboard>()}
         , _m_clock{pb->lookup_impl<RelativeClock>()}
         , _m_slow_pose{sb->get_reader<pose_type>("slow_pose")}
         , _m_imu_raw{sb->get_reader<imu_raw_type>("imu_raw")}
         , _m_true_pose{sb->get_reader<pose_type>("true_pose")}
         , _m_ground_truth_offset{sb->get_reader<switchboard::event_wrapper<Eigen::Vector3f>>("ground_truth_offset")}
-		, _m_vsync_estimate{sb->get_reader<switchboard::event_wrapper<time_point>>("vsync_estimate")}
+        , _m_vsync_estimate{sb->get_reader<switchboard::event_wrapper<time_point>>("vsync_estimate")}
     { }
 
     // No parameter get_fast_pose() predicts to the next vsync estimate, if one exists, otherwise predicts to 'now'
@@ -36,21 +36,21 @@ public:
 
         pose_type offset_pose;
 
-		// Subtract offset if valid pose and offset, otherwise use zero pose.
-		// Checking that pose and offset are both valid is safer than just
-		// checking one or the other because it assumes nothing about the
-		// ordering of writes on the producer's end or about the producer
-		// actually writing to both streams.
-		if (pose_ptr != nullptr && offset_ptr != nullptr) {
-			offset_pose             = *pose_ptr;
-			offset_pose.position   -= **offset_ptr;
-		} else {
-			offset_pose.sensor_time = _m_clock->now();
-			offset_pose.position    = Eigen::Vector3f{0, 0, 0};
-			offset_pose.orientation = Eigen::Quaternionf{1, 0, 0, 0};
-		}
+        // Subtract offset if valid pose and offset, otherwise use zero pose.
+        // Checking that pose and offset are both valid is safer than just
+        // checking one or the other because it assumes nothing about the
+        // ordering of writes on the producer's end or about the producer
+        // actually writing to both streams.
+        if (pose_ptr != nullptr && offset_ptr != nullptr) {
+            offset_pose             = *pose_ptr;
+            offset_pose.position   -= **offset_ptr;
+        } else {
+            offset_pose.sensor_time = _m_clock->now();
+            offset_pose.position    = Eigen::Vector3f{0, 0, 0};
+            offset_pose.orientation = Eigen::Quaternionf{1, 0, 0, 0};
+        }
 
-		return correct_pose(offset_pose);
+        return correct_pose(offset_pose);
     }
 
 
@@ -81,7 +81,7 @@ public:
 
         // Both slow_pose and imu_raw are available, do pose prediction
         double dt = duration2double(future_timestamp - imu_raw->imu_time);
-        std::pair<Eigen::Matrix<double,13,1>, time_point> predictor_result = predict_mean_rk4(dt);
+        std::pair<Eigen::Matrix<double, 13, 1>, time_point> predictor_result = predict_const_vel(dt);
         auto state_plus = predictor_result.first;
 
         // predictor_imu_time is the most recent IMU sample that was used to compute the prediction.
@@ -200,97 +200,46 @@ private:
     const std::shared_ptr<const RelativeClock> _m_clock;
     switchboard::reader<pose_type> _m_slow_pose;
     switchboard::reader<imu_raw_type> _m_imu_raw;
-	switchboard::reader<pose_type> _m_true_pose;
+    switchboard::reader<pose_type> _m_true_pose;
     switchboard::reader<switchboard::event_wrapper<Eigen::Vector3f>> _m_ground_truth_offset;
     switchboard::reader<switchboard::event_wrapper<time_point>> _m_vsync_estimate;
-	mutable Eigen::Quaternionf offset {Eigen::Quaternionf::Identity()};
-	mutable std::shared_mutex offset_mutex;
+    mutable Eigen::Quaternionf offset {Eigen::Quaternionf::Identity()};
+    mutable std::shared_mutex offset_mutex;
 
-    // Slightly modified copy of OpenVINS method found in propagator.cpp
+    // Based on https://github.com/rpng/open_vins/issues/92
     // Returns a pair of the predictor state and the time associated with the
     // most recent imu reading used to perform this prediction.
-    std::pair<Eigen::Matrix<double,13,1>,time_point> predict_mean_rk4(double dt) const {
+    std::pair<Eigen::Matrix<double, 13, 1>, time_point> predict_const_vel(double dt) const {
+        const auto imu_raw = _m_imu_raw.get_ro();
 
-        // Pre-compute things
-        switchboard::ptr<const imu_raw_type> imu_raw = _m_imu_raw.get_ro();
+        // Current state
+        const auto curr_time = imu_raw->imu_time;
+        // TODO: should this be the last w or the estimated w in the VIO/integrator state?
+        const auto curr_w    = 0.5 * (imu_raw->w_hat + imu_raw->w_hat2);
+        const auto curr_quat = imu_raw->quat;
+        const auto curr_R    = quat_2_Rot(Eigen::Matrix<double, 4, 1>{curr_quat.x(), curr_quat.y(), curr_quat.z(), curr_quat.w()});
+        const auto curr_pos  = imu_raw->pos;
+        const auto curr_vel  = imu_raw->vel;
 
-        Eigen::Vector3d w_hat =imu_raw->w_hat;
-        Eigen::Vector3d a_hat = imu_raw->a_hat;
-        Eigen::Vector3d w_alpha = (imu_raw->w_hat2-imu_raw->w_hat)/dt;
-        Eigen::Vector3d a_jerk = (imu_raw->a_hat2-imu_raw->a_hat)/dt;
+        // Compute new quaternion
 
-        // y0 ================
-        Eigen::Quaterniond temp_quat = imu_raw->quat;
-        Eigen::Vector4d q_0 = {temp_quat.x(), temp_quat.y(), temp_quat.z(), temp_quat.w()};
-        Eigen::Vector3d p_0 = imu_raw->pos;
-        Eigen::Vector3d v_0 = imu_raw->vel;
+        // Option 1: PG's code from issue 92. Same as Propagator.cpp
+        const auto future_q = rot_2_quat(exp_so3(-curr_w * dt) * curr_R);
 
-        // k1 ================
-        Eigen::Vector4d dq_0 = {0,0,0,1};
-        Eigen::Vector4d q0_dot = 0.5*Omega(w_hat)*dq_0;
-        Eigen::Vector3d p0_dot = v_0;
-        Eigen::Matrix3d R_Gto0 = quat_2_Rot(quat_multiply(dq_0,q_0));
-        Eigen::Vector3d v0_dot = R_Gto0.transpose()*a_hat-Eigen::Vector3d{0.0, 0.0, 9.81};
+        // Option 2: PG's formula from issue 92. Same as Trawny eq. 96.
+        // const auto future_q = rot_2_quat(exp_so3(0.5 * Omega(curr_w) * dt) * curr_R);
 
-        Eigen::Vector4d k1_q = q0_dot*dt;
-        Eigen::Vector3d k1_p = p0_dot*dt;
-        Eigen::Vector3d k1_v = v0_dot*dt;
+        // Compute new position
+        const auto delta_pos = curr_vel * dt;
+        const auto future_pos = curr_pos + delta_pos;
 
-        // k2 ================
-        w_hat += 0.5*w_alpha*dt;
-        a_hat += 0.5*a_jerk*dt;
+        // Assemble state
+        Eigen::Matrix<double, 13, 1> state_plus = Eigen::Matrix<double, 13, 1>::Zero();
+        state_plus.block(0, 0, 4, 1) = future_q;
+        state_plus.block(4, 0, 3, 1) = future_pos;
+        state_plus.block(7, 0, 3, 1) = curr_vel;
 
-        Eigen::Vector4d dq_1 = quatnorm(dq_0+0.5*k1_q);
-        Eigen::Vector3d v_1 = v_0+0.5*k1_v;
-
-        Eigen::Vector4d q1_dot = 0.5*Omega(w_hat)*dq_1;
-        Eigen::Vector3d p1_dot = v_1;
-        Eigen::Matrix3d R_Gto1 = quat_2_Rot(quat_multiply(dq_1,q_0));
-        Eigen::Vector3d v1_dot = R_Gto1.transpose()*a_hat-Eigen::Vector3d{0.0, 0.0, 9.81};
-
-        Eigen::Vector4d k2_q = q1_dot*dt;
-        Eigen::Vector3d k2_p = p1_dot*dt;
-        Eigen::Vector3d k2_v = v1_dot*dt;
-
-        // k3 ================
-        Eigen::Vector4d dq_2 = quatnorm(dq_0+0.5*k2_q);
-        //Eigen::Vector3d p_2 = p_0+0.5*k2_p;
-        Eigen::Vector3d v_2 = v_0+0.5*k2_v;
-
-        Eigen::Vector4d q2_dot = 0.5*Omega(w_hat)*dq_2;
-        Eigen::Vector3d p2_dot = v_2;
-        Eigen::Matrix3d R_Gto2 = quat_2_Rot(quat_multiply(dq_2,q_0));
-        Eigen::Vector3d v2_dot = R_Gto2.transpose()*a_hat-Eigen::Vector3d{0.0, 0.0, 9.81};
-
-        Eigen::Vector4d k3_q = q2_dot*dt;
-        Eigen::Vector3d k3_p = p2_dot*dt;
-        Eigen::Vector3d k3_v = v2_dot*dt;
-
-        // k4 ================
-        w_hat += 0.5*w_alpha*dt;
-        a_hat += 0.5*a_jerk*dt;
-
-        Eigen::Vector4d dq_3 = quatnorm(dq_0+k3_q);
-        //Eigen::Vector3d p_3 = p_0+k3_p;
-        Eigen::Vector3d v_3 = v_0+k3_v;
-
-        Eigen::Vector4d q3_dot = 0.5*Omega(w_hat)*dq_3;
-        Eigen::Vector3d p3_dot = v_3;
-        Eigen::Matrix3d R_Gto3 = quat_2_Rot(quat_multiply(dq_3,q_0));
-        Eigen::Vector3d v3_dot = R_Gto3.transpose()*a_hat-Eigen::Vector3d{0.0, 0.0, 9.81};
-
-        Eigen::Vector4d k4_q = q3_dot*dt;
-        Eigen::Vector3d k4_p = p3_dot*dt;
-        Eigen::Vector3d k4_v = v3_dot*dt;
-
-        // y+dt ================
-        Eigen::Matrix<double,13,1> state_plus = Eigen::Matrix<double,13,1>::Zero();
-        Eigen::Vector4d dq = quatnorm(dq_0+(1.0/6.0)*k1_q+(1.0/3.0)*k2_q+(1.0/3.0)*k3_q+(1.0/6.0)*k4_q);
-        state_plus.block(0,0,4,1) = quat_multiply(dq, q_0);
-        state_plus.block(4,0,3,1) = p_0+(1.0/6.0)*k1_p+(1.0/3.0)*k2_p+(1.0/3.0)*k3_p+(1.0/6.0)*k4_p;
-        state_plus.block(7,0,3,1) = v_0+(1.0/6.0)*k1_v+(1.0/3.0)*k2_v+(1.0/3.0)*k3_v+(1.0/6.0)*k4_v;
-
-        return {state_plus, imu_raw->imu_time};
+        return {state_plus, curr_time};
     }
 
     /**
@@ -309,18 +258,6 @@ private:
     }
 
     /**
-     * @brief Normalizes a quaternion to make sure it is unit norm
-     * @param q_t Quaternion to normalized
-     * @return Normalized quaterion
-     */
-    static const inline Eigen::Matrix<double, 4, 1> quatnorm(Eigen::Matrix<double, 4, 1> q_t) {
-        if (q_t(3, 0) < 0) {
-            q_t *= -1;
-        }
-        return q_t / q_t.norm();
-    }
-
-    /**
      * @brief Skew-symmetric matrix from a given 3x1 vector
      *
      * This is based on equation 6 in [Indirect Kalman Filter for 3D Attitude Estimation](http://mars.cs.umn.edu/tr/reports/Trawny05b.pdf):
@@ -336,9 +273,9 @@ private:
      */
     static const inline Eigen::Matrix<double, 3, 3> skew_x(const Eigen::Matrix<double, 3, 1> &w) {
         Eigen::Matrix<double, 3, 3> w_x;
-        w_x << 0, -w(2), w(1),
-                w(2), 0, -w(0),
-                -w(1), w(0), 0;
+        w_x <<     0, -w(2),  w(1),
+                w(2),     0, -w(0),
+               -w(1),  w(0),     0;
         return w_x;
     }
 
@@ -362,55 +299,112 @@ private:
     }
 
     /**
-     * @brief Multiply two JPL quaternions
+     * @brief Returns a JPL quaternion from a rotation matrix
      *
-     * This is based on equation 9 in [Indirect Kalman Filter for 3D Attitude Estimation](http://mars.cs.umn.edu/tr/reports/Trawny05b.pdf).
-     * We also enforce that the quaternion is unique by having q_4 be greater than zero.
+     * This is based on the equation 74 in [Indirect Kalman Filter for 3D Attitude Estimation](http://mars.cs.umn.edu/tr/reports/Trawny05b.pdf).
+     * In the implementation, we have 4 statements so that we avoid a division by zero and
+     * instead always divide by the largest diagonal element. This all comes from the
+     * definition of a rotation matrix, using the diagonal elements and an off-diagonal.
      * \f{align*}{
-     *  \bar{q}\otimes\bar{p}=
-     *  \mathcal{L}(\bar{q})\bar{p}=
+     *  \mathbf{R}(\bar{q})=
      *  \begin{bmatrix}
-     *  q_4\mathbf{I}_3+\lfloor\mathbf{q}\times\rfloor & \mathbf{q} \\
-     *  -\mathbf{q}^\top & q_4
+     *  q_1^2-q_2^2-q_3^2+q_4^2 & 2(q_1q_2+q_3q_4) & 2(q_1q_3-q_2q_4) \\
+     *  2(q_1q_2-q_3q_4) & -q_2^2+q_2^2-q_3^2+q_4^2 & 2(q_2q_3+q_1q_4) \\
+     *  2(q_1q_3+q_2q_4) & 2(q_2q_3-q_1q_4) & -q_1^2-q_2^2+q_3^2+q_4^2
      *  \end{bmatrix}
-     *  \begin{bmatrix}
-     *  \mathbf{p} \\ p_4
-     *  \end{bmatrix}
-     * @f}
+     * \f}
      *
-     * @param[in] q First JPL quaternion
-     * @param[in] p Second JPL quaternion
-     * @return 4x1 resulting p*q quaternion
+     * @param[in] rot 3x3 rotation matrix
+     * @return 4x1 quaternion
      */
-    static const inline Eigen::Matrix<double, 4, 1> quat_multiply(const Eigen::Matrix<double, 4, 1> &q, const Eigen::Matrix<double, 4, 1> &p) {
-        Eigen::Matrix<double, 4, 1> q_t;
-        Eigen::Matrix<double, 4, 4> Qm;
-        // create big L matrix
-        Qm.block(0, 0, 3, 3) = q(3, 0) * Eigen::MatrixXd::Identity(3, 3) - skew_x(q.block(0, 0, 3, 1));
-        Qm.block(0, 3, 3, 1) = q.block(0, 0, 3, 1);
-        Qm.block(3, 0, 1, 3) = -q.block(0, 0, 3, 1).transpose();
-        Qm(3, 3) = q(3, 0);
-        q_t = Qm * p;
-        // ensure unique by forcing q_4 to be >0
-        if (q_t(3, 0) < 0) {
-            q_t *= -1;
+    static const inline Eigen::Matrix<double, 4, 1> rot_2_quat(const Eigen::Matrix<double, 3, 3> &rot) {
+        Eigen::Matrix<double, 4, 1> q;
+        double T = rot.trace();
+        if ((rot(0, 0) >= T) && (rot(0, 0) >= rot(1, 1)) && (rot(0, 0) >= rot(2, 2))) {
+            q(0) = sqrt((1 + (2 * rot(0, 0)) - T) / 4);
+            q(1) = (1 / (4 * q(0))) * (rot(0, 1) + rot(1, 0));
+            q(2) = (1 / (4 * q(0))) * (rot(0, 2) + rot(2, 0));
+            q(3) = (1 / (4 * q(0))) * (rot(1, 2) - rot(2, 1));
+
+        } else if ((rot(1, 1) >= T) && (rot(1, 1) >= rot(0, 0)) && (rot(1, 1) >= rot(2, 2))) {
+            q(1) = sqrt((1 + (2 * rot(1, 1)) - T) / 4);
+            q(0) = (1 / (4 * q(1))) * (rot(0, 1) + rot(1, 0));
+            q(2) = (1 / (4 * q(1))) * (rot(1, 2) + rot(2, 1));
+            q(3) = (1 / (4 * q(1))) * (rot(2, 0) - rot(0, 2));
+        } else if ((rot(2, 2) >= T) && (rot(2, 2) >= rot(0, 0)) && (rot(2, 2) >= rot(1, 1))) {
+            q(2) = sqrt((1 + (2 * rot(2, 2)) - T) / 4);
+            q(0) = (1 / (4 * q(2))) * (rot(0, 2) + rot(2, 0));
+            q(1) = (1 / (4 * q(2))) * (rot(1, 2) + rot(2, 1));
+            q(3) = (1 / (4 * q(2))) * (rot(0, 1) - rot(1, 0));
+        } else {
+            q(3) = sqrt((1 + T) / 4);
+            q(0) = (1 / (4 * q(3))) * (rot(1, 2) - rot(2, 1));
+            q(1) = (1 / (4 * q(3))) * (rot(2, 0) - rot(0, 2));
+            q(2) = (1 / (4 * q(3))) * (rot(0, 1) - rot(1, 0));
+        }
+        if (q(3) < 0) {
+            q = -q;
         }
         // normalize and return
-        return q_t / q_t.norm();
+        q = q / (q.norm());
+        return q;
+    }
+
+    /**
+     * @brief SO(3) matrix exponential
+     *
+     * SO(3) matrix exponential mapping from the vector to SO(3) lie group.
+     * This formula ends up being the [Rodrigues formula](https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula).
+     * This definition was taken from "Lie Groups for 2D and 3D Transformations" by Ethan Eade equation 15.
+     * http://ethaneade.com/lie.pdf
+     *
+     * \f{align*}{
+     * \exp\colon\mathfrak{so}(3)&\to SO(3) \\
+     * \exp(\mathbf{v}) &=
+     * \mathbf{I}
+     * +\frac{\sin{\theta}}{\theta}\lfloor\mathbf{v}\times\rfloor
+     * +\frac{1-\cos{\theta}}{\theta^2}\lfloor\mathbf{v}\times\rfloor^2 \\
+     * \mathrm{where}&\quad \theta^2 = \mathbf{v}^\top\mathbf{v}
+     * @f}
+     *
+     * @param[in] w 3x1 vector we will take the exponential of
+     * @return SO(3) rotation matrix
+     */
+    static const inline Eigen::Matrix<double, 3, 3> exp_so3(const Eigen::Matrix<double, 3, 1> &w) {
+        // get theta
+        Eigen::Matrix<double, 3, 3> w_x = skew_x(w);
+        double theta = w.norm();
+        // Handle small angle values
+        double A, B;
+        if(theta < 1e-12) {
+            A = 1;
+            B = 0.5;
+        } else {
+            A = sin(theta)/theta;
+            B = (1-cos(theta))/(theta*theta);
+        }
+        // compute so(3) rotation
+        Eigen::Matrix<double, 3, 3> R;
+        if (theta == 0) {
+            R = Eigen::MatrixXd::Identity(3, 3);
+        } else {
+            R = Eigen::MatrixXd::Identity(3, 3) + A * w_x + B * w_x * w_x;
+        }
+        return R;
     }
 };
 
-class const_accel_predictor_plugin : public plugin {
+class const_vel_predictor_plugin : public plugin {
 public:
-    const_accel_predictor_plugin(const std::string& name, phonebook* pb)
+    const_vel_predictor_plugin(const std::string& name, phonebook* pb)
         : plugin{name, pb}
     {
         pb->register_impl<pose_prediction>(
             std::static_pointer_cast<pose_prediction>(
-                std::make_shared<const_accel_predictor_impl>(pb)
+                std::make_shared<const_vel_predictor_impl>(pb)
             )
         );
     }
 };
 
-PLUGIN_MAIN(const_accel_predictor_plugin);
+PLUGIN_MAIN(const_vel_predictor_plugin);
